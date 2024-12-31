c41335df48ed1e3e7c69dccb65e8529da9c798fd
MrAdamTaylor <admataylorgb@gmail.com>
2024-12-31 17:51:04 +0300

    - Finish First Lesson
    - Finish work with Lesson of Reflection

--- Assets/LessonReflection/Launch.cs
+++ Assets/LessonReflection/Launch.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 
 public class Launch : MonoBehaviour
 {
-    
-    
     private void Start()
     {
         var currentAssembly = Assembly.GetExecutingAssembly();
@@ -12,9 +10,23 @@ public class Launch : MonoBehaviour
         foreach (var type in typesFromCurrentAssembly)
         {
             Debug.Log(type.Name);
+            foreach (var method in type.GetMethods())
+            {
+                Debug.Log($"Method of type {type.Name} a {method}");
+            }
         }
 
         var typePerson = currentAssembly.GetType("Person");
         Debug.Log($"Current type {typePerson}!");
+
+        foreach (var constructor in typePerson.GetConstructors())
+        {
+            Debug.Log($"Is Person Constructor: {constructor}");
+        }
+
+        foreach (var info in typePerson.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+        {
+            Debug.Log($"Is Person NoPublic: {info}");
+        }
     }
 }

7fac3a9bb42970bd83ecf096ea4efadd304e3ce8
MrAdamTaylor <admataylorgb@gmail.com>
2025-02-04 19:28:35 +0300

    Invoke Method with reflection
    - change fields
    - invoke methods

--- Assets/ReflectionMonitoring/Demo/ObjectManipulator.cs
+++ Assets/ReflectionMonitoring/Demo/ObjectManipulator.cs
@@ -26,13 +26,62 @@ public class ObjectManipulator : MonoBehaviour
 
         var person4 = Activator.CreateInstance(typeof(Person));
 
-        var person5 = Activator.CreateInstance(typeof(Person), BindingFlags.Instance | BindingFlags.Public, null, new object[] {"Kevin"}, null);
+        var person5 = Activator.CreateInstance(typeof(Person), BindingFlags.Instance | BindingFlags.Public, 
+            null, new object[] {"Kevin"}, null);
 
         var assembly = Assembly.GetExecutingAssembly();
         var person8 = assembly.CreateInstance("ReflectionMonitoring.Demo.Person", true, 
             BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, 
             new object[] {"Lediberd", 60}, null, null);
+
+        #region Create with methods GetPropery and GetField
+        Debug.Log($"Person name before Reflection - {person8}");
+        var nameProperty = person8?.GetType().GetProperty("Name");
+        nameProperty?.SetValue(person8,"Sergey");
+
+
+        var ageField = person8.GetType().GetField("_aPrivateField", 
+            BindingFlags.Instance | BindingFlags.NonPublic);
+        ageField?.SetValue(person8, "Private Field");
+        #endregion
+        
+        
+        #region CreateObjectByInterface
+
+            var actualTypeFromConfig = Type.GetType(GetTypeFromConfiguration());
+            var iTalkInstance = Activator.CreateInstance(actualTypeFromConfig) as ITalk;
+            iTalkInstance.Talk("Hello man!");
+        
+        #endregion
+        
+        #region InvokeMember
+
+        person5?.GetType().InvokeMember("Name", 
+            BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty, null, 
+            person5, new[] {"Emma"});
+
+        #endregion
+
+        #region Methods
+
+        var talkMethod = person8?.GetType().GetMethod("Talk");
+        talkMethod?.Invoke(person8, new[] { "Invoking: Talk in parallel" });
+
+        person5?.GetType().InvokeMember("Yell",
+            BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, null, person5,
+            new[] { "Have a constructive dialogue" });
+
+        #endregion
+        
+        Debug.Log($"New Person8 a {person5}");
+        Debug.Log($"New Person5 a {person5}");
         
         Debug.Log("Finish!");
+    }
+
+
+    static string GetTypeFromConfiguration()
+    {
+        return "ReflectionMonitoring.Demo.Person";
     }
 }

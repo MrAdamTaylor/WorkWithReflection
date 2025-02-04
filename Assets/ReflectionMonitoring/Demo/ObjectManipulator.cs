1f330bfd5ce7ffe83d3fa5cc1e66f20acc090437
MrAdamTaylor <admataylorgb@gmail.com>
2025-02-04 16:15:42 +0300

    Dynamic Creating objects
    - dynamic creating with Activator and Assembly

--- Assets/ReflectionMonitoring/Demo/ObjectManipulator.cs
+++ Assets/ReflectionMonitoring/Demo/ObjectManipulator.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Reflection;
+using ReflectionMonitoring.Demo;
 using UnityEngine;
 
 public class ObjectManipulator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
-    }
+        var personType = typeof(Person);
+        var personConstructors = 
+            personType.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
 
-    // Update is called once per frame
-    void Update()
-    {
+        foreach (var constructor in personConstructors)
+        {
+            Debug.Log($"Constructor: {constructor}");
+        }
+
+        var privatePersonConstructor = personType.GetConstructor(
+            BindingFlags.Instance | BindingFlags.Public, null, 
+            new Type[] { typeof(string), typeof(int)}, null);
+
+        var person1 = personConstructors[0].Invoke(null);
+        var person2 = personConstructors[1].Invoke(new object[] {"Kevin"});
+        var person3 = personConstructors[2].Invoke(new object[] {"Kevin", 40});
+
+        var person4 = Activator.CreateInstance(typeof(Person));
+
+        var person5 = Activator.CreateInstance(typeof(Person), BindingFlags.Instance | BindingFlags.Public, null, new object[] {"Kevin"}, null);
+
+        var assembly = Assembly.GetExecutingAssembly();
+        var person8 = assembly.CreateInstance("ReflectionMonitoring.Demo.Person", true, 
+            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, 
+            new object[] {"Lediberd", 60}, null, null);
         
+        Debug.Log("Finish!");
     }
 }

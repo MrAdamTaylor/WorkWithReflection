79f1be5663796f0ac50a9fffac0c6c78a63887dd
MrAdamTaylor <admataylorgb@gmail.com>
2025-03-03 18:52:26 +0300

    Load Unversioned Files

---
+++ Assets/Serialization/LoadConstants.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class LoadConstants 
+{
+    public const string JSON_FILE_NAME = "WeatherForecast.json";
+    public const string JSON_FILE_PATH = "/Serialization/LoadedFiles";
+}
